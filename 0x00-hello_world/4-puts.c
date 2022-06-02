d41b472fc9a0213034b985fe37b4eebed6be0056
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-02 14:49:16 +0300

    multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - print Programming is like building a multilingual puzzle
- * return: 0 if success, otherwise not 0
+ * return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
