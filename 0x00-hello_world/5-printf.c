64ec2028d5b0a4815dea7454426b21a53189a076
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-02 06:05:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print with proper grammar, but the outcome is a piece of art, 
+ * main - print with proper grammar, but the outcome is a piece of art
  * Return: always 0
  */
 int main(void)
