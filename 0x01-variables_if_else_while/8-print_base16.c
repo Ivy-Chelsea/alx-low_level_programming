4f4844c4badedbf033d82809082672d9fad6a837
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-03 03:09:27 -0700

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - print numbers base 16
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 10; g < 20; g++)
+	{
+		putchar(g);
+	}
+	for (g = 22; g < 28; g++)
+	{
+		putchar(g);
+	}
+	putchar('\n');
+	return (0);
+}
