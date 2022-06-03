f81ec977873cf78e0f7e71638b4a515a6e9d76aa
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-04 00:59:41 +0300

    more combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,24 @@
 int main(void)
 {
 	int i, e;
-	
-	for ( i = 0; i <= 98; i++)
+
+	for (i = 0; i <= 98; i++)
 	{
-		for ( e = i + 1; e <= 99; e++)
+		for (e = i + 1; e <= 99; e++)
 		{
-			putchar((i /10) + '0');
-		 	putchar((i % 10) + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((e / 10) + '0');
 			putchar((e % 10) + '0');
-			
+
 			if (i == 98 && e == 99)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
