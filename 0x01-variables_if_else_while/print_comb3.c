a6a9fd739d6f0279b480fc39ba7eafc3b2034ddf
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-03 03:30:53 -0700

    2-digit combination

---
+++ 0x01-variables_if_else_while/print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - combinations of 2 digits
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, v;
+
+	for (i = 48; i <=56; i++)
+	{
+		for (v = 49; v <= 57; v++)
+		{
+			if (v > i)
+			{
+				putchar(i);
+				putchar(v);
+				if (i != 56 || v!= 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
