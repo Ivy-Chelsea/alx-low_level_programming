85ff8b2d41876a4ec73d42d684a30ce253f78e2e
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-03 03:53:33 -0700

    3 digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+/**
+ * main - print 3 digit combination
+ *
+ * Return - always 0
+ */
+int main(void)
+{
+	int i, v, y;
+
+	i=28;
+	v=28;
+	y=28;
+
+	while (v < 38)
+	{
+		i = 28;
+		while (i < 38)
+		{
+			y = 28;
+			while (y < 38)
+			{
+				if (v != i && v != y && i != y && v < i && i < y)
+				{
+					putchar(v);
+					putchar(i);
+					putchar(y);
+					if (i == 36 && v == 35 && y == 37)
+					{
+						break
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				y++;
+			}
+			i++;
+		}
+		v++;
+	}
+	putchar('\n');
+	return (0);
+}
