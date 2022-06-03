cc9116ce7ea0bd66a3ce4bcc71ea8360cbca9673
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-03 23:26:28 +0300

    combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int h;
 
-	for (h = 5; h < 15; h++)
+	for (h = 0; h <= 9; h++)
 	{
-		putchar(h);
-		if (h != 14)
+		putchar((h % 10) + '0');
+		if (h == 9)
 		{
-			putchar(' ');
 			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
