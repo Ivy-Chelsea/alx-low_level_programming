c5cc17fba66664d7b0b332b0542f2e06697ad0ef
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-03 01:47:04 -0700

    upper and lower case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print lowercase then uppercase alphabets
+ * 
+ * Return: always 0
+ */
+int main(void)
+{
+	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int b;
+
+	for (b = 0; b < 52; b++)
+	{
+		putchar(alph[b]);
+	}
+	putchar('\n');
+	return(0);
+}
