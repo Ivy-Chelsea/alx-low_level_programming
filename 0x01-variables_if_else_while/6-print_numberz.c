f067945f311dc2f928cd7b03d3a79231d372bd35
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-03 02:40:39 -0700

    <=e<10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - print from 0 to 9
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int e;
+	
+	for (e = 0; e < 10; e++)
+	{
+		putchar(e);
+	}
+	putchar('\n');
+	return (0);
+}
