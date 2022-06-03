7919fd612992e76114f4626236bb50f57e61560f
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-03 01:41:27 -0700

    lowercaseletters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Print lower case alphabets
+ * Return: always 0
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghojklmnopqrstuvwxyz";
+	int a;
+	
+	for (a = 0; a < 26; a++)
+	{
+		putchar(alph[a]);
+	}
+	putchar('\n');
+	return (0);
+}
