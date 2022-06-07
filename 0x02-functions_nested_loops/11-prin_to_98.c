f72a639c37e489a1de47f5d506ad17443b4a0fd9
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-07 05:08:48 -0700

    print n-98

---
+++ 0x02-functions_nested_loops/11-prin_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ *
+ * @n: type number
+ *
+ * Return: 98
+ */
+void print_to_98(int n)
+{
+	int o;
+
+	for (0 = n; 0 != 98;)
+
+	{
+		printf("%D, ", 0);
+
+		if (n > 98)
+			0--;
+		else
+			0++;
+	}
+	printf("98\n");
+}
