d8812239150d609f1957a13fd8a8c6f7a8dd8789
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-07 04:23:24 -0700

    24hrs

---
+++ 0x02-functions_nested_loops/8-2dhours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes of the day
+ * From 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int n, g;
+
+	while (n < 24)
+	{
+		g = 0;
+		while (g < 60)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(':');
+			_putchar((g / 10) + '0');
+			_putchar((g % 10) + '0');
+			_putchar('\n');
+			g++;
+		}
+		n++;
+	}
+}
