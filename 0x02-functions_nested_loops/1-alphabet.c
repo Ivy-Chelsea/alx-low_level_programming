02210245fa1bde868569168ae70592245c448d7d
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-07 02:19:43 -0700

    a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet a-z
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char j = 'a';
+	int m;
+
+	for (m = o; m < 26; m++)
+	{
+		_putchar(j + m);
+	}
+	_putchar(10);
+}
