e8892c7096971b8a534d606c6ee37c0911ccbe8b
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-07 19:19:12 +0300

    test +/-

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *
  * * main - tests function that prints if integer is positive or negative
@@ -10,22 +8,12 @@
  *
  * */
 
-
-
 int main(void)
-
 {
-
-	        int i;
-
-
-
-		        i = 0;
-
-			        positive_or_negative(i);
-
-
-
-				        return (0);
-
+	inti;
+	
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
 }
