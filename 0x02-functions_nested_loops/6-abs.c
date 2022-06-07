b2f29401d3c49bd9cb86789cd87a47e021e36ff2
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-07 03:26:37 -0700

    determomine absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ *
+ * @g: int type number
+ *
+ * Return: absolute value of @g
+ */
+int _abs(int g)
+{
+	if (g < 0)
+	{
+		return (g * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
