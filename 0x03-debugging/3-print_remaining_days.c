90a274e3ea8185fe6f64148d1bec54e047865b07
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-08 23:40:09 +0300

    365 days this day

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,6 +9,7 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
@@ -30,7 +31,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n". 365 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
