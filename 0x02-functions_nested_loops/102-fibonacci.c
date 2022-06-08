6e83362374516a78274e7ef595d939a108da2751
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-08 05:17:19 -0700

    fibonacci0

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int i, j, k, next;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			print("%1d, ", j);
+		}
+		else
+		{
+			printf("%1d\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
