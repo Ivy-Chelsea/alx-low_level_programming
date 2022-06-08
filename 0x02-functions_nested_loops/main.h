bd547fe99f8626c05a9a131e185d1e817c54e1c9
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-08 21:20:38 +0300

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int q, int x);
 void print_to_98(int n);
-void print_times_table(int n);
+void times_table(int n);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n)
