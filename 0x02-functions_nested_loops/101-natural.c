4db189f45faba7be50bd2cfffea8ee4691321641
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-08 04:47:20 -0700

    sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints sum of multiples
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long int sum3, sum5, sum;
+	int z;
+
+	for (z = 0; z < 1024; z++)
+	{
+		if ((z % 3) == 0)
+		{
+			sum3 = sum3 + z;
+		}
+		else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	
+	return (0);
+}
