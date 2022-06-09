95272417d4cf30e87f0fe2b766e07ac900eb8c99
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-09 21:38:06 +0300

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,24 +8,17 @@
  */
 void print_diagonal(int n)
 {
+	int x, y;
+	
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
-	else
+	for (x = 0; x < n; x++)
 	{
-		int x, y;
-		
-		for (x = 0; x < n; x++)
+		for (y = 0; y < x; y ++)
 		{
-			for (y = 0; y < x; y ++)
-			{
-				if (y < x)
-					_putchar(' ');
-				if else (y == x)
-					_putchar('\\');
-			}
-		_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
