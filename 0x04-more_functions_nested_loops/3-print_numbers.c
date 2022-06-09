f63dafe8ecd25f32eb54c18921339c50e9f02df2
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-09 04:51:05 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0-9
+ * followed by new line
+ */
+void print_numbers(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+	{
+		_putchar(p + '0');
+	}
+	_putchar('\n');
+}
