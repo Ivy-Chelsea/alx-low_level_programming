c12a78fe239740e10bf6973e99f9e4a8535d72ef
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-09 13:36:20 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("School\n");
+	}
+	return 0;
+}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,24 @@
  */
 void print_diagonal(int n)
 {
-	int x, y;
-
 	if (n <= 0)
-		_putchar('\n');
-	for (x = 0; x < n; x++)
 	{
-		for (y = 0; y < x; y ++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		int x, y;
+
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				if (y == x)
+					_putchar('\\');
+				else if (y < x)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
 	}
 }
