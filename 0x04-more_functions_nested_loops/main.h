4223e663a2ea635423ad09e7c20ac06fe66fa1c4
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-09 14:55:31 +0300

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numberers(void);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
