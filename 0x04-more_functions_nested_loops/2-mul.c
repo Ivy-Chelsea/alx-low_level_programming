eb728a4694eec8762f8735c7d503e385ea340bad
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-09 04:46:32 -0700

    multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: type number
+ * @b: type number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int y;
+
+	y = a * b;
+
+	return (y);
+}
