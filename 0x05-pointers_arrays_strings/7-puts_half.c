d6aca90095e440eb72421d658ae7aca06f1ab6e4
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-13 06:06:07 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half string
+ * @str: the string
+ */
+void puts_half(char *str)
+{
+	int a, b, c;
+
+	a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	if (a % 2 == 0)
+	{
+		for (c = a / 2 ; str[c] != '\0'; c++)
+		{
+			_putchar(str[c]);
+		}
+	}
+	else if (a % 2)
+	{
+		for (b = (a - 1) / 2; b < a - 1; b++)
+		{
+			_putchar(str[b + 1]);
+		}
+	}
+	_putchar('\n');
+}
