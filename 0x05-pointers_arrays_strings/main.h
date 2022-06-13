cb5ef85317cbe4378e946457bd29627936d8184e
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-13 19:17:15 +0300

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -12,5 +12,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
