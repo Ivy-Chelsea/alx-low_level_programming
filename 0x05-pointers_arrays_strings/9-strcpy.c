2dd1f3ee99ac6cb01e4c5a5c0222268a6a47f6e7
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-13 06:25:33 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string to pointer
+ * @dest: destination
+ * @src: string
+ *
+ * Return: pointer destinatiom
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int b, l;
+
+	b = 0;
+
+	while (scr[b] != '\0')
+	{
+		b++;
+	}
+
+	for (l = 0; l < b; l++)
+	{
+		dest[l] = src[l];
+	}
+	dest[l] = '\0';
+
+	return (dest);
+}
