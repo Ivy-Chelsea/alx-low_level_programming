84fd301e0ccb40af9744d745ea5e344411635e90
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-15 08:19:21 -0700

    start

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to the string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
