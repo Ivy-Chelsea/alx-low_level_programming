1d7828ee93f32ffbd2d62f14b5f4b0d631078c09
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-16 13:00:14 +0300

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
+ * infinite_add -  adds two numbers
  * @n1: first number
  * @n2: second number
  * @r: result
@@ -10,37 +10,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	/*local variable declaration */
-	int i = 0, j = 0; k, l = 0, f, s, d = 0;
+	/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
 	while (n1[i] != '\0')
 		i++;
+	while (n2[j] != '\0')
+		j++;
 	if (i > j)
 		l = i;
-	else l = j;
+	else
+		l = j;
 	if (l + 1 > size_r)
 		return (0);
-	r[1] = '\0';
-	for (k = l - 1; k >= 0; k--)
+		r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
 		i--;
 		j--;
 		if (i >= 0)
 			f = n1[i] - '0';
 		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
 			s = 0;
-		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
+			r[k] = (f + s + d) % 10 + '0';
+			d = (f + s + d) / 10;
 	}
 	if (d == 1)
 	{
 		r[l + 1] = '\0';
 		if (l + 2 > size_r)
-			return (0);
+		return (0);
 		while (l-- >= 0)
 			r[l + 1] = r[l];
 			r[0] = d + '0';
 	}
-
 	return (r);
 }
