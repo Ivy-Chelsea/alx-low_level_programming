7a314fdbcfb227c851b88add482e7d04a87ed038
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-24 19:54:14 +0300

    sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	{
 		for (p = 0; argv[y][p]; p++)
 		{
-			if (argv[y][p] < 0 || argv[y][p] > 9)
+			if (argv[y][p] < '0' || argv[y][p] > '9')
 			{
 				print("Error\n");
 				return (1);
