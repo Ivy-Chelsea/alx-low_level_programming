8fb873a401c512802363d5b23ac4637b8b74fc08
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-24 08:40:29 -0700

    prog name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints programme's name, followed by new line
+ * @argc: number of arguments
+ * @argv: array of pointers to the string
+ * Return: 0
+ */
+int main(int argc, int argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
