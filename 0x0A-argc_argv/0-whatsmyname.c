c81c4acea187309fa10fb5aeb4d10324ddd95c53
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-24 18:45:01 +0300

    Prog name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: array of pointers to the string
  * Return: 0
  */
-int main(int argc, int argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%s\n", *argv);
 
 	return (0);
 }
