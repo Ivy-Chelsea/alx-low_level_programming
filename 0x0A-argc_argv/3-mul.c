58e345bfe90db6e615e624dc7d02cab5e5540912
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-24 19:34:48 +0300

    product

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two numbers
@@ -8,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int y, p, mul = 0;
+	int y, p, mul;
 
 	if (argc != 3)
 	{
