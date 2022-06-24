125c57ff08e34e440aa5e3fa822c205e8e3f4099
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-06-24 09:31:38 -0700

    product

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of pointers to string
+ * Return: 1 and type error for error,0 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int y, p, mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	y = atoi(argv[1]);
+	p = atoi(argv[2]);
+	mul = y * p;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
