5233b7827ec8bf1fff05e809ba21d5f5c9d6f857
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-06-30 17:12:05 +0300

    main file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef FILE_MAIN
 #define FIE_MAIN
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
