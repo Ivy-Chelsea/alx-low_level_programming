a73b13a1f7b4191525eb5ce03c055ad5f92e55ae
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-07-06 14:48:07 -0700

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,7 +14,7 @@ int main(int argc, char  *argv[])
 
 	if (argc != 2)
 	{
-		printf("ERROR\N");
+		printf("Error\n");
 		exit(1);
 	}
 
@@ -22,7 +22,7 @@ int main(int argc, char  *argv[])
 
 	if (bytes < 0)
 	{
-		print("Error\n");
+		printf("Error\n");
 		exit(2);
 	}
 
