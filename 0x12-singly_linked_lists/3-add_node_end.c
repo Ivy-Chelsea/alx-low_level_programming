ced7a099cf82f394fde31794a187d531fdafc8e8
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-07-14 15:47:16 +0300

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,8 +11,8 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t *temp = *head
-		unsigned int len = 0;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
 	while (str[len])
 		len++;
