1581adc41bbbabb70840265bb16314026ca47151
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-07-18 09:37:10 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of elements in a linked list
+ * @h: linked list to traverse
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0
+
+		while (h)
+		{
+			num++;
+			h = h->next;
+		}
+
+		return (num);
+}
