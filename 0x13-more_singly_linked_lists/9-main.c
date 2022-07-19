902b584140395f1e2f3e19c280fe89d361112f5c
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-07-19 04:21:54 -0700

    delete node

--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -22,7 +22,9 @@ int main(void)
 	add_nodeint_end(&head, 402;
 	add_nodeint_end(&head, 1024);
 	sum = sum_listint(head);
-	printf("sum = %d\n", sum);
+	printf("----------------\n");
+	insert_nodeint_at_index(&head, 5, 4096);
+	print_listint(head);
 	free_listint2(&head);
 	return (0);
 }
