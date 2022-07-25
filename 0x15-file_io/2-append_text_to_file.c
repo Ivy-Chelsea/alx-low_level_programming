d4f862a4aedbe85d5c315e27f2a353ba157596fb
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-07-25 09:52:23 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
 
