4b4d883d95757dc4a7d0d4bcfddfe616cb052f71
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-22 19:50:10 +0300

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void print_list(hash_node_t *h)
  * hash_table_print - prints a hash table
  * @ht: hash table to print
  */
-voi hash_table_print(const hash_table_t *ht)
+void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node = NULL;
