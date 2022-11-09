69105cfd67dc9b074241f25eba5c24383bff157b
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-09 18:37:22 +0300

    school file

--- 0x1C-makefiles/school.c
+++ 0x1C-makefiles/school.c
@@ -1,9 +1,9 @@
 #include m.h"
 
 /**
- * print_school - entry point
- * Return: nothing
- */
+* print_school - entry point
+* Return: nothing
+*/
 void print_school(void)
 {
 	printf("j#0000000000000000000000000000000000000\n");
